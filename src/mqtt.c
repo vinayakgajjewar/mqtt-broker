7e8f225f15c8e093b06195b48630b69798bb9dca
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2025-08-03 10:41:42 -0700

    done w/ mqtt protocol stuff

--- src/mqtt.c
+++ src/mqtt.c
@@ -101,4 +101,119 @@ static size_t unpack_mqtt_connect(const unsigned char *buf, union mqtt_header *h
     return len;
 }
 
-// TODO: unpack_mqtt_publish()
+static size_t unpack_mqtt_publish(const unsigned char *buf, union mqtt_header *hdr, union mqtt_packet *pkt) {
+    struct mqtt_publish publish = {.header = *hdr};
+    pkt->publish = publish;
+    size_t len = mqtt_decode_length(&buf);
+    pkt->publish.topiclen = unpack_string16((uint8_t **) &buf, &pkt->publish.topic);
+    uint16_t message_len = len;
+    if (publish.header.bits.qos > AT_MOST_ONCE) {
+        pkt->publish.pkt_id = unpack_u16((const uint8_t **) &buf);
+        message_len -= sizeof(uint16_t);
+    }
+    message_len -= (sizeof(uint16_t) + pkt->publish.topiclen);
+    pkt->publish.payloadlen = message_len;
+    pkt->publish.payload = malloc(message_len + 1);
+    unpack_bytes((const uint8_t **) &buf, message_len, pkt->publish.payload);
+    return len;
+}
+
+static size_t unpack_mqtt_subscribe(const unsigned char *buf, union mqtt_header *hdr, union mqtt_packet *pkt) {
+    struct mqtt_subscribe subscribe = {.header = *hdr};
+    size_t len = mqtt_decode_length(&buf);
+    size_t remaining_bytes = len;
+    subscribe.pkt_id = unpack_u16((const uint8_t **) &buf);
+    remaining_bytes -= sizeof(uint16_t);
+
+    /*
+     * The payload consists of a list of 3-tuples consisting of:
+     * - Topic length
+     * - Topic filter string
+     * - QoS
+     */
+    int i = 0;
+    while (remaining_bytes > 0) {
+        remaining_bytes -= sizeof(uint16_t);
+
+        /*
+         * TODO
+         * Might leak memory here if realloc() fails.
+         */
+        subscribe.tuples = realloc(subscribe.tuples, (i + 1) * sizeof(*subscribe.tuples));
+        subscribe.tuples[i].topic_len = unpack_string16(&buf, &subscribe.tuples[i].topic);
+        remaining_bytes -= subscribe.tuples[i].topic_len;
+        subscribe.tuples[i].qos = unpack_u8((const uint8_t **) &buf);
+        len -= sizeof(uint8_t);
+        i++;
+    }
+    subscribe.tuples_len = i;
+    pkt->subscribe = subscribe;
+    return len;
+}
+
+static size_t unpack_mqtt_unsubcribe(const unsigned char *buf, union mqtt_header *hdr, union mqtt_packet *pkt) {
+    struct mqtt_unsubscribe unsubscribe = {.header = *hdr};
+    size_t len = mqtt_decode_length(&buf);
+    size_t remaining_bytes = len;
+    unsubscribe.pkt_id = unpack_u16((const uint8_t **) &buf);
+    remaining_bytes -= sizeof(uint16_t);
+
+    /*
+     * The payload consists of a list of 2-tuples containing:
+     * - Topic length
+     * - Topic filter string
+     */
+    int i = 0;
+    while (remaining_bytes > 0) {
+        remaining_bytes -= sizeof(uint16_t);
+
+        /*
+         * TODO
+         * Might leak memory here if realloc() fails.
+         */
+        unsubscribe.tuples = realloc(unsubscribe.tuples, (i + 1) * sizeof(*unsubscribe.tuples));
+        unsubscribe.tuples[i].topic_len = unpack_string16((uint8_t **) &buf, &unsubscribe.tuples[i].topic);
+        remaining_bytes -= unsubscribe.tuples[i].topic_len;
+        i++;
+    }
+    unsubscribe.tuples_len = i;
+    pkt->unsubscribe = unsubscribe;
+    return len;
+}
+
+/*
+ * The below packets are literally just a header and a packet ID.
+ * - PUBACK
+ * - PUBREC
+ * - PUBREL
+ * - PUBCOMP
+ * - UNSUBACK
+ */
+static size_t unpack_mqtt_ack(const unsigned char *buf, union mqtt_header *hdr, union mqtt_packet *pkt) {
+    struct mqtt_ack ack = {.header = *hdr};
+    size_t len = mqtt_decode_length(&buf);
+    ack.pkt_id = unpack_u16((const uint8_t **) &buf);
+    pkt->ack = ack;
+    return len;
+}
+
+typedef size_t mqtt_unpack_handler(const unsigned char *, union mqtt_header *, union mqtt_packet *);
+
+static mqtt_unpack_handler *unpack_handlers[11] = {NULL, unpack_mqtt_connect, NULL, unpack_mqtt_publish,
+                                                   unpack_mqtt_ack, unpack_mqtt_ack, unpack_mqtt_ack, unpack_mqtt_ack,
+                                                   unpack_mqtt_subscribe, NULL, unpack_mqtt_unsubscribe};
+
+/*
+ * Call the appropriate unpacking function depending on the header.
+ */
+int unpack_mqtt_packet(const unsigned char *buf, union mqtt_packet *pkt) {
+    int rc = 0;
+    unsigned char type = *buf;
+    union mqtt_header header = {.byte = type};
+    if (header.bits.type == DISCONNECT || header.bits.type == PINGREQ || header.bits.type == PINGRESP) {
+        pkt->header = header;
+    } else {
+        rc = unpack_handlers[header.bits.type](++buf, &header, pkt);
+        return rc;
+    }
+}
